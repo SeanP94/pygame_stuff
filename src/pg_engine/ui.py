708f10be6c834297d028f39e6a276b5218b075f9
Sean Perry <Seanep94@gmail.com>
2025-09-06 20:44:39 -0700

    Added more ui - see weapon on screen in box

--- src/pg_engine/ui.py
+++ src/pg_engine/ui.py
@@ -1,5 +1,7 @@
 import pygame
 from user_input_data import uinp
+from .globals import GRAPHICS_DIR
+
 
 BAR_HEIGHT = 20
 HEALTH_BAR_WIDTH = 200
@@ -23,6 +25,9 @@ class UI:
         # bar setup
         self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
+
+        # Import weapon data images.
+        self.weapon_graphics = [pygame.image.load(GRAPHICS_DIR / x['graphic']).convert_alpha() for x in uinp['weapon_data'].values()]
     
     def show_bar(self, stat, stat_max, bg_rect, color):
         # draw background and outline.
@@ -51,11 +56,28 @@ class UI:
 
         self.display_surface.blit(text_surf, text_rect)
 
-    def selection_box(self, left, top):
-        pass
+    def selection_box(self, left, top, has_switched):
+        bg_rect = pygame.Rect(left, top, ITEM_BOX_SIZE, ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+        if not has_switched:
+            pygame.draw.rect(self.display_surface, UI_BORDER_COLOR_ACTIVE, bg_rect, 3)
+        else:
+            pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
+        return bg_rect
+
+    def weapon_overlay(self, weapon_index, has_switched):
+        bg_rect = self.selection_box(10, 625, has_switched) # Equiped weapon
+        
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center=bg_rect.center)
+
+        self.display_surface.blit(weapon_surf, weapon_rect)
+        
 
     def display(self, player):
         self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
         self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
         
         self.show_exp(player.exp)
+        self.weapon_overlay(player.weapon_index, player.can_switch_weapon)
+        # self.selection_box(80, 635)
